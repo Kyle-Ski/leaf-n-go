5a4f3404f8df953fa4c853c31e671b31d4c8c2f6
Kyle-Ski <skiroyjenkins@gmail.com>
2024-11-15 09:50:36 -0700

    getting cookies correctly if we need them for later

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
-import { cookies } from 'next/headers';
+// import { cookies } from 'next/headers';
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import { Database } from '@/types/projectTypes';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 /**
  * This function will handle extracting and validating the user information from the session
@@ -9,7 +9,11 @@ import { NextRequest } from 'next/server';
  * @returns user id from the session
  */
 export async function getUserFromSession(req: NextRequest) {
-  const supabase = createMiddlewareClient<Database>({ req, cookies });
+  // Extract cookies to use for session management if we nee them later
+  // const cookieStore = cookies();
+  
+  // Create the Supabase client with the request and response
+  const supabase = createMiddlewareClient<Database>({ req, res: NextResponse.next() });
 
   // Get session information from Supabase
   const {
@@ -17,7 +21,7 @@ export async function getUserFromSession(req: NextRequest) {
   } = await supabase.auth.getSession();
 
   if (session && session.user) {
-    return session.user.id; // Return the user ID if session is valid
+    return session.user.id; // Return the user ID if the session is valid
   } else {
     return null; // No valid session, return null
   }
