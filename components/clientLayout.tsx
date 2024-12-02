811c7579d581a1bd4526739ea747480890e3fc10
Kyle-Ski <skiroyjenkins@gmail.com>
2024-12-02 10:59:31 -0800

    making it so that unauthed users dont see nav bars

--- components/clientLayout.tsx
+++ components/clientLayout.tsx
@@ -4,27 +4,34 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { BottomNav } from "@/components/bottomNav";
 import { usePathname } from "next/navigation";
+import { useAuth } from "@/lib/auth-Context";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const { user } = useAuth();
+
+  const alignmentClasses = user
+    ? "max-w-screen-sm sm:max-w-screen-md lg:max-w-screen-lg px-4 pb-20"
+    : "flex items-center justify-center min-h-screen";
 
   return (
     <SidebarProvider>
       {/* Sidebar for desktop and tablet */}
-      <div className="hidden md:flex">
-        <AppSidebar />
-        <SidebarTrigger />
-      </div>
-
+      {user && (
+        <div className="hidden md:flex">
+          <AppSidebar />
+          <SidebarTrigger />
+        </div>)
+      }
       {/* Main Content with Spinner */}
       <main className="flex-grow p-4 md:p-6 lg:p-8 bg-gray-50 relative">
-        <div className={`max-w-screen-sm sm:max-w-screen-md lg:max-w-screen-lg w-full px-4 pb-20 "opacity-100"`}>
+        <div className={alignmentClasses}>
           {children}
         </div>
       </main>
 
       {/* Bottom Navigation for mobile */}
-      <BottomNav currentPath={pathname} />
+      {user && <BottomNav currentPath={pathname} />}
     </SidebarProvider>
   );
 }
