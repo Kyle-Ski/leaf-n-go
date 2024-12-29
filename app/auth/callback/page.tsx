0b15e47f051d2adac29c1735a40201d986db5ea9
Kyle-Ski <skiroyjenkins@gmail.com>
2024-12-29 08:26:32 -0800

    vercel issue?

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -64,7 +64,7 @@ export default function AuthCallback() {
       setTimeout(() => router.push("/auth"), 2000);
     }
   };
-
+//diff
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-4">
       <h1 className="text-2xl font-semibold mb-4">Authentication</h1>
